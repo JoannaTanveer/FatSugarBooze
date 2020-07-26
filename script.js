c93e414851edbd91756a6fbcec9e643007435b51
Banana <banana.computermonkey@gmail.com>
2020-07-25 19:55:28 -0700

    #4 Spoonacular API WIP jt

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-var apiKey = "1"
-var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+var apiKeyBooze = "1"
+var queryURLBooze = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
 
 $.ajax({
-    url: queryURL,
+    url: queryURLBooze,
     method: "GET"
 }).then(function(response) {
     console.log(response)
+})
+
+
+const apiKeySpoon = "f5609a52a62f4012a1dbb5e2733393c4";
+
+var queryURLSpoon = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKeySpoon}?ingredients=apples,+flour,+sugar&number=2`;
+
+$.ajax({
+    url: queryURLSpoon,
+    method: "GET"
+}).then(function(response) {
+    console.log(response, "fart")
 })
